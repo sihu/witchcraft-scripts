82e7c573effcbe878743aa359d6bc6fc13494be8
Josua Schmid <josua.schmid@renuo.ch>
2018-10-26 12:03:02 +0200

    Catch error if config isn't present

--- _config.js
+++ _config.js
@@ -1,6 +1,15 @@
-const config =
-  // @include .config.json
-;
+let config = {};
+
+try {
+  eval(`
+    config =
+      // @include .config.json
+    ;
+  `);
+}
+catch(err) {
+  throw 'There is something wrong with your configuration. Did you create a file called ".config.json"?';
+}
 
 function callIfEnabled(configKey, callback) {
   if(config[configKey]) {

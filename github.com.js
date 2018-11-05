47aa907cac1e3b06b94ca186440bfb6412de5744
Simon Huber <simon.huber@renuo.ch>
2018-11-05 12:17:10 +0100

    Improve structure

--- github.com.js
+++ github.com.js
@@ -1,14 +1,23 @@
 // @include _config.js
 callIfEnabled('github.com', function () {
   (function () {
-    if (window.location.href.includes('pull')) {
+    function addRedmineTicketLink() {
       const featureTag = document.querySelector('.commit-ref.head-ref');
       const redmineId = featureTag.textContent.match(/\d+/)[0];
       const redmineUrl = `https://redmine.renuo.ch/issues/${redmineId}`;
       featureTag.innerHTML = `<a href="${redmineUrl}" target="_blank">${featureTag.textContent}</a>`
+    }
 
+    function makeFileRemovalsMoreVisible() {
       const moreVisibleFileRemoval = "<span style='color:red;'>Watch out! 👀 File deleted!!!</span>";
-      document.querySelectorAll('span[aria-label="Binary file removed"').forEach(function(el) { el.innerHTML += moreVisibleFileRemoval })
+      document.querySelectorAll('span[aria-label="Binary file removed"').forEach(function(el) {
+        el.innerHTML += moreVisibleFileRemoval
+      })
+    }
+
+    if (window.location.href.includes('pull')) {
+      addRedmineTicketLink();
+      makeFileRemovalsMoreVisible();
     }
   })();
 });
